65e460f63b9860dcfa6173c35cb3f6b0db6440a5
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-04-02 19:44:18 +0100

    more practice, more tests

--- specs/my_function_specs2.rb
+++ specs/my_function_specs2.rb
@@ -24,7 +24,6 @@ end
 
 
 
-
 # Dr luther king speech
 def test_dr_king_speech()
   string1 = "I have a dream "
@@ -33,6 +32,22 @@ def test_dr_king_speech()
     assert_equal("I have a dream that one day", speech_result)
 end
 
+
+
+def test_am_i_safe_to_drive()
+  drunk_or_not = too_drunk_to_drive (50)
+  assert_equal(true, drunk_or_not)
+end
+
+def test_how_long_is_the_string()
+  string_length = string_length(20,50)
+  assert_equal(70,string_length)
+end
+
+def test_sweets_eaten()
+  sweets_remaining = sweets_remaining (50,20)
+  assert_equal(30, sweets_remaining)
+end
 
 
 
