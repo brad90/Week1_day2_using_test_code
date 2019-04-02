486e5915faab2185f2fede68835cb18a6f785d63
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-04-02 19:22:40 +0100

    further testing added to file

--- specs/my_function_specs2.rb
+++ specs/my_function_specs2.rb
@@ -1,17 +1,37 @@
-require ('minitest/autorun')
-require ('minitest/rg')
-require_relative ('../my_functions2')
+require( 'minitest/autorun' )
+require( 'minitest/rg')
+require_relative( '../my_functions2')
+
+
+class FunctionsTest < MiniTest::Test
+
+
+#the answer is 1000
+
+def test_adds_up_to_1000 ()
+  result1000 = return_addition_1000(450 , 550)
+  assert_equal( 1000, result1000 )
+end
 
 
 
-class
+# the area of a square
+
+def test_area_of_square()
+  area_square_result = area_of_square(5)
+  assert_equal(25,area_square_result)
+end
 
 
-# Give the value of 100 hundred back
 
-# Find the area of a triangle
 
-#Give abraham lincolns address
+# Dr luther king speech
+def test_dr_king_speech()
+  string1 = "I have a dream "
+  string2 = "that one day"
+  speech_result = speech_full(string1, string2)
+    assert_equal("I have a dream that one day", speech_result)
+end
 
 
 
