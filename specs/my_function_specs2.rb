9e2679a4c68f0d111a307274471f26e7e8a19642
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-04-02 18:39:33 +0100

    made new test file to practice on my own

---
+++ specs/my_function_specs2.rb
@@ -0,0 +1,19 @@
+require ('minitest/autorun')
+require ('minitest/rg')
+require_relative ('../my_functions2')
+
+
+
+class
+
+
+# Give the value of 100 hundred back
+
+# Find the area of a triangle
+
+#Give abraham lincolns address
+
+
+
+
+end
